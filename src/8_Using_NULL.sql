2d2c889289a8ba78c4b217556b46463ec85778ce
ykmaku <dsm4up2c@gmail.com>
2020-06-14 00:54:26 +0900

    8_Using_NULL

---
+++ src/8_Using_NULL.sql
@@ -0,0 +1,54 @@
+-- 1
+SELECT name
+FROM teacher
+WHERE dept IS NULL
+
+-- 2
+SELECT teacher.name, dept.name
+FROM teacher INNER JOIN dept
+    ON (teacher.dept=dept.id)
+
+-- 3
+SELECT teacher.name, dept.name
+FROM teacher LEFT JOIN dept
+  ON teacher.dept=dept.id
+
+--4
+SELECT teacher.name, dept.name
+FROM dept LEFT JOIN teacher
+  ON dept.id=teacher.dept
+
+-- 5
+SELECT name, COALESCE(mobile, '07986 444 2266') AS mobile
+FROM teacher
+
+-- 6
+SELECT teacher.name, COALESCE(dept.name, 'None')
+FROM teacher LEFT JOIN dept
+  ON teacher.dept=dept.id
+
+-- 7
+SELECT COUNT(name), COUNT(mobile)
+FROM teacher
+
+-- 8
+SELECT dept.name, COUNT(teacher.name)
+FROM dept LEFT JOIN teacher
+  ON dept.id=teacher.dept
+GROUP BY dept.name
+
+-- 9
+SELECT name, 
+        (CASE WHEN dept IN (1,2) THEN 'Sci'
+            ELSE 'Art'
+        END)
+FROM teacher
+
+-- 10
+SELECT name, 
+        (CASE WHEN dept IN (1,2) THEN 'Sci'
+              WHEN dept IN (3) THEN 'Art'
+            ELSE 'None'
+        END)
+FROM teacher
+
